85384f8d61d21f57dc84a28861fef409a1cd8d39
Sam <samantha.bogen@edu.sait.ca>
2023-09-22 10:09:23 -0600

    week 3 fix
    fix

--- app/week3/item.js
+++ app/week3/item.js
@@ -1,4 +1,4 @@
-export default function item({ name, quantity, category }) {
+export default function Item({ name, quantity, category }) {
    return (
     <div className="border border-sky-500 bg-sky-800 w-full max-w-xs m-4 p-2">
       <h3 className="text-xl font-bold">{name}</h3>
