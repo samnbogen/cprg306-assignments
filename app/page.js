e23254eacf550ebf60fa41f4b82b589f568fc4f6
Sam <samantha.bogen@edu.sait.ca>
2023-10-11 20:21:28 -0600

    week5

--- app/page.js
+++ app/page.js
@@ -10,6 +10,7 @@ export default function Page() {
         <li><Link href="/week2">Week 2</Link></li>
         <li><Link href="/week3">Week 3</Link></li>
         <li><Link href="/week4">Week 4</Link></li>
+        <li><Link href="/week5">Week 5</Link></li>
       </ul>
     </div>
   );
